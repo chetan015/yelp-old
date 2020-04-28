958613a979388c19cea5d64453aec541a9c91154
Chetan Surana <37964089+chetan015@users.noreply.github.com>
2020-04-27 20:15:36 -0700

    Added tooltip on hover.

--- heatmap.js
+++ heatmap.js
@@ -6,19 +6,16 @@ async function createHeatMap(businessId){
     Object.keys(checkIns).forEach(i =>Object.values(checkIns[i]).forEach(j=>checkInList.push(j)));
     
     // set margins, width
-    var margin = { top: 90, right: 50, bottom: 60, left: 70};
-    offsetWidth = parseInt(d3.select(".Rectangle_9").style("width").slice(0,-2));
-    offsetHeight = parseInt(d3.select(".Rectangle_9").style("height").slice(0,-2));
-    console.log(offsetWidth);
+    var margin = { top: 50, right: 0, bottom: 60, left: 120 },
+    offsetWidth = parseInt(d3.select("#heatMapArea").style("width").slice(0,-2))
     //offsetWidth = 800
-    width = 1.0*offsetWidth - margin.left - margin.right;
-    height = 1.0*offsetHeight - margin.top - margin.bottom;
-    // boxSize = Math.floor(width / 24)
-    boxSize = 37;
-    legendBoxWidth = boxSize;
-    dayLabelList = ["M", "T", "W", "TH", "F", "SA", "SU"];
+    width = 0.8*offsetWidth - margin.left - margin.right
+    height = 0.45*width - margin.top - margin.bottom
+    boxSize = Math.floor(width / 24),
+    legendBoxWidth = boxSize*1.5,
+    dayLabelList = ["M", "T", "W", "TH", "F", "SA", "SU"],
     timeLabelList = ["1a", "2a", "3a", "4a", "5a", "6a", "7a", "8a", "9a", "10a", "11a", "12p", "1p", "2p", "3p", "4p", "5p", "6p", "7p", "8p", "9p", "10p", "11p"];
-    var svg = d3.select(".Rectangle_9").append("svg")
+    var svg = d3.select("#heatMapArea").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
@@ -49,7 +46,8 @@ async function createHeatMap(businessId){
                       
     var colorScale = d3.scaleThreshold()
         .domain(thresholds)
-        .range(d3.schemeYlGnBu[9]);
+        .range(d3.schemeYlGnBu[9])
+        ;
     var boxes = svg.selectAll(".box")
         .data(checkInList);  
         boxes.enter().append("rect")
@@ -71,20 +69,18 @@ async function createHeatMap(businessId){
     // legend.append("g");
     // var legendBox = legend.selectAll("g");
     legend.data([0].concat(thresholds)).enter().append("rect")
-        .attr("x",function(d,i){return width/4+legendBoxWidth*i})
-        .attr("y",height-35)
+        .attr("x",function(d,i){return 150+legendBoxWidth*i})
+        .attr("y",height)
         .attr("width",legendBoxWidth)
-        .attr("height",legendBoxWidth/2.2)
+        .attr("height",legendBoxWidth/2)
         .style("fill","white")
         .attr("class","legendBox")
         .transition().duration(function(d,i){return 300 + i*170;})
         .style("fill",function(d){return colorScale(d);});
 
-    // legend.data(["<2"].concat(thresholds.slice(1)).concat(">=100")).enter().append('text')
-    
     legend.data(["<2"].concat(thresholds.slice(1)).concat(">=100")).enter().append('text')
-    .attr("x",function(d,i){val = width/4.1+legendBoxWidth*(i+1);if(i==7)val-=5; else if(i==8) val-=20;return val})
-    .attr("y",height-40)
+    .attr("x",function(d,i){val = 140+legendBoxWidth*(i+1);if(i==7)val-=5; else if(i==8) val-=20;return val})
+    .attr("y",height+40)
     .text(function(d){return d;})
     .style("font-size","10px")
     .style("font-weight","bold")
